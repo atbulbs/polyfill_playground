bb61915006a7d855da65799cbcf0c0e7471c053b
liaoxiaomin <liaoxiaomin@vipkid.com.cn>
2019-11-25 17:27:06 +0800

    my_async

---
+++ lib/types/test/main.d.ts
@@ -0,0 +1,3 @@
+declare function _axios(num: any): Promise<unknown>;
+declare function myAsync(generatorFn: any): (data: any) => void;
+declare function G(n: any): Generator<Promise<unknown>, void, unknown>;
---
+++ test/main.ts
@@ -0,0 +1,46 @@
+// // 模拟axios
+// function _axios (num): Promise<any> {
+//   return new Promise(resolve => {
+//     setTimeout(() => {
+//       resolve(++num)
+//     })
+//   })
+// }
+
+// function myAsync (generatorFn) {
+//   function asyncFun (...params: any[]) {
+//     const args = Array.prototype.slice.call(arguments, 0)
+//     const iterator = generatorFn.apply(this, args)
+//     function autoRun (data) {
+//       const result = iterator.next(data)
+//       if (result.done){
+//         return result.value
+//       } else {
+//         result.value.then(value => {
+//           autoRun(value)
+//         })
+//       }
+//     }
+//     autoRun.apply(this, args)
+//   }
+//   return asyncFun
+// }
+
+// // Generator 函数
+// function * G (n: number) {
+//   const res1 = yield _axios(n)
+//   console.log(res1) // 2
+//   const res2 = yield _axios(res1)
+//   console.log(res2) // 3
+// }
+
+// myAsync(G)(1)
+
+// async function _G (n: number) {
+//   const res1 = await _axios(n)
+//   console.log(res1) // 2
+//   const res2 = await _axios(res1)
+//   console.log(res2) // 3
+// }
+
+// _G(1)
